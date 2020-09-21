031fb302c1c1355df43eb529689679adc240ce21
stiven-capry <70813592+stiven-capry@users.noreply.github.com>
2020-09-20 20:31:47 -0500

    sgg

--- punto2.php
+++ punto2.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>2°Punto</title>
+    
 </head>
 <body>
     <form action="punto2.php">
+
+    
     </form>
 </body>
 </html>
